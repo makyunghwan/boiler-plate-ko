81cb68bec58211bed36577a08a332b5cd05ddc99
마경환 <magyeonghwan@magyeonghwan-ui-MacBookAir.local>
2020-07-13 21:56:50 +0900

    증명기능

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const {User} = require("./models/User");
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/User');
+const { auth } = require('./middleware/auth');
+
 
 const config = require('./config/key');
 
+//app./x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
+//app./jsaon
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const user = require('./models/User');
 mongoose.connect(config.mongoURI, {
     useNewUrlParse: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected!'))
@@ -23,25 +29,71 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello World~~ 새해복많이 !!!'))
 
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
-    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
-    //그것들을 데이터 베이스에 넣어준다.
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
+app.post('/api/users/login', (req, res) => {
 
-    const user = new User(req.body)
-
-    user.save((err, userInfo) => {
-      if(err) return res.json({ success: false, err })
-      return res.status(200).json({
-        success: true
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
       })
+    }
+  
+
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({loginSuccess: false, message: "비밀번호가 틀렸씁니다."})
+        
+      //비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        //토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id}) 
+
+
+      })
+    })
+  })
+})
+
+//Router -> Express
+
+app.get('/api/users/auth', auth, (req, res) => {
+
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true라는 말.
+    res.status(200).json({
+      _id: req.user._id,
+      isAdmin: req.user.role === 0 ? false : true,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+      lastname: req.user.lastname,
+      role: req.user.role,
+      image: req.user.image
+
     })
 
 
 })
-
-
 
 
 
